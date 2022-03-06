fda11d31d8f986f2e7fcdb3d5dd8a92b00510355
Alex Yazmin Manzo Olguin <ada_hack418@hackademy.mx>
2022-03-06 17:07:36 -0600

    cambios

--- registro_apunte.php
+++ registro_apunte.php
@@ -33,6 +33,8 @@ $query_insert = mysqli_query($conection,"INSERT INTO usuario(nombre,correo,usuar
 <html>
     <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include "scripts.php" ?>
         <title>REGISTRO USUARIO</title>
         <link rel="stylesheet" type="css"" href="style2.css">
